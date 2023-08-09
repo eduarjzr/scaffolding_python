5b09ec19d8037236f268343e0c05c20698da3f06
eduardo.zapata <eduardo.zapata@wayops.eu>
2023-08-09 14:23:06 +0200

    test whit start

--- scaffolding_python/__init__.py
+++ scaffolding_python/__init__.py
@@ -1,6 +1,7 @@
 from scaffolding_python.fibonacci_exercise.fibonacci import generate_fibonacci_series as fibonacci
 from scaffolding_python.combinations_coins.combination_coins import combinations_coins as coins
 from scaffolding_python.configs.config_logger import configure_logging
+from scaffolding_python.combinations_coins.__main__ import start
 
 configure_logging()
 
